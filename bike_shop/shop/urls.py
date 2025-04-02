2bc5ec70fd6674e8d97f8a58eb32786fa60a7630
Seregun <seregunmgp@gmail.com>
2025-04-02 22:59:31 +0300

    Add files via upload

--- bike_shop/shop/urls.py
+++ bike_shop/shop/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('shop/', views.shop_info, name='shop_info'),
+    path('spec/', views.spec_list, name='spec_list'),
+    path('spec/<int:spec_id>/', views.spec_detail, name='spec_detail'),
 ]
--- bike_shop/shop/views.py
+++ bike_shop/shop/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
+import json
+import os
+
+def load_specs():
+    json_path = os.path.join(os.path.dirname(__file__), 'dump.json')
+    try:
+        with open(json_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
 def home(request):
     """Представление для главной страницы"""
@@ -11,3 +22,18 @@ def about(request):
 def shop_info(request):
     """Представление для страницы о магазине"""
     return render(request, 'shop/shop_info.html')
+
+def spec_list(request):
+    """Представление для списка квалификаций"""
+    specs = load_specs()
+    return render(request, 'shop/spec_list.html', {'specs': specs})
+
+def spec_detail(request, spec_id):
+    """Представление для детальной информации о квалификации"""
+    specs = load_specs()
+    spec = next((s for s in specs if s.get('id') == spec_id), None)
+
+    if spec is None:
+        raise Http404("Квалификация не найдена")
+
+    return render(request, 'shop/spec_detail.html', {'spec': spec})
