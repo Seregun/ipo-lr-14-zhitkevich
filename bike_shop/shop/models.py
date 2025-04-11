c5a8fcf4e0bb6f8e244c7511f081d2c8808b73dc
Seregun <seregunmgp@gmail.com>
2025-04-11 23:03:49 +0300

    Delete bike_shop directory

--- bike_shop/shop/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Qualification(models.Model):
-    qualification_id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return f"{self.qualification_id} - {self.name}"
